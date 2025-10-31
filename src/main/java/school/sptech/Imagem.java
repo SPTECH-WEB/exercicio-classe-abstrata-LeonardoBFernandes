8e588acc8f81442eacbd552dff0094b6e9766832
LeonardoBFernandes <leonardo.fernandes@sptech.school>
2025-10-31 12:21:57 -0300

    Subindo atividade realizada

--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,42 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+    private List<Figura> figuras;
+
+    public Imagem() {}
+
+    public void adicionar(Figura figura) {
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas() {
+        Double soma = 0.0;
+        for (Figura f:figuras) {
+            soma += f.calcularArea();
+        }
+        return soma;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20() {
+        List<Figura> figurasComAreaMaiorQue20 = new ArrayList<>();
+        for (Figura f:figuras) {
+            if (f.calcularArea() > 20) {
+                figurasComAreaMaiorQue20.add(f);
+            }
+        }
+        return figurasComAreaMaiorQue20;
+    }
+
+    public List<Figura> buscarQuadrados() {
+        List<Figura> quadrados = new ArrayList<>();
+        for (Figura f:figuras) {
+            if (f instanceof Quadrado) {
+                quadrados.add(f);
+            }
+        }
+        return quadrados;
+    }
 }
--- src/main/java/school/sptech/Triangulo.java
+++ src/main/java/school/sptech/Triangulo.java
@@ -1,4 +1,37 @@
 package school.sptech;
 
-public class Triangulo {
+public class Triangulo extends Figura{
+    private Double base;
+    private Double altura;
+
+    public Triangulo() {}
+
+    @Override
+    public Double calcularArea() {
+        return (base * altura) / 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangulo{" +
+                "base=" + base +
+                ", altura=" + altura +
+                '}';
+    }
+
+    public Double getBase() {
+        return base;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setBase(Double base) {
+        this.base = base;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
 }
